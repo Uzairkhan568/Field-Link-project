8a17ec9b0d4587a4fc6d7f79bb4e177f8b44ea27
Uzair Khan <sincerelyuzairkhan@gmail.com>
2026-08-21 16:34:15 +0530

    Set up backend server and API connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>FIELDLINK</h1>
+      <p>{message}</p>
     </div>
   );
 }
